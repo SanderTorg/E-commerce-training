9c7e06fe959854cded510b3ea908d6ff9acd4e16
SanderTorg <santorgersen@gmail.com>
2025-03-20 14:21:19 +0100

    deleting stuff

--- src/cart.mjs
+++ src/cart.mjs
@@ -6,25 +6,15 @@ import { createHTML, clearNode } from "./utils.mjs";
 const cartToggleBtnEL = document.querySelector("#js-cart-toggle");
 const cartContainerEl = document.querySelector("#js-cart");
 const cartCloseBtnEl = document.querySelector("#js-close-cart");
-const containerItemsEl = document.querySelector("#js-cart-products");
-const clearCartBtnEl = document.querySelector("#js-clear-cart");
-const totalCartPriceEl = document.querySelector("#js-total");
 
 setup();
 
 function setup() {
-  if (
-    !cartToggleBtnEL ||
-    !cartContainerEl ||
-    !cartCloseBtnEl ||
-    !containerItemsEl ||
-    !clearCartBtnEl
-  ) {
+  if (!cartToggleBtnEL || !cartContainerEl || !cartCloseBtnEl) {
     console.error("JS is CRACKED!");
   } else {
     cartToggleBtnEL.addEventListener("click", toggleCartAside);
     cartCloseBtnEl.addEventListener("click", toggleCartAside);
-    clearCartBtnEl.addEventListener("click", clearAllItemsFromCart);
 
     const products = getItemsFromStorage();
     renderCartItems(products);
@@ -33,56 +23,6 @@ function setup() {
 
 function toggleCartAside() {
   cartContainerEl.classList.toggle("is-open");
-}
-
-function cartTemplate({
-  id,
-  imgUrl = "",
-  title = "Unknown",
-  price = 0,
-  alt = "No Alt provided",
-}) {
-  return `
-  <article class="c-cart-container"> 
-    <div class="c-cart-col-1">
-      <a href="/product-details.html?id=${id}">
-        <img src="${imgUrl}" alt="${alt}  class="c-cart-img"/>
-      </a>
-    </div>  
-    <div class="c-cart-col-2">
-        <a href="/product-details.html?id=${id}">
-          <h3>${title}</h3>
-        </a>
-        <strong class="c-cart-item-price">${price} ${CURRENCY}</strong>
-        <button class="c-cart-btn-remove" item-btn="removeCartItem" id="${id}">Remove</button>
-    </div>
-  </article>
-  `;
-}
-
-function renderCartItems(items = []) {
-  clearNode(containerItemsEl);
-
-  items.forEach(({ id, imgUrl, title, price }) => {
-    const template = cartTemplate({
-      id,
-      title,
-      imgUrl,
-      price,
-    });
-
-    const createNewEl = createHTML(template);
-
-    const removeBtnEl = createNewEl.querySelector(
-      '[item-btn="removeCartItem"]'
-    );
-
-    removeBtnEl.addEventListener("click", (event) => {
-      removeItemFromCart(items, event.target.id);
-    });
-
-    containerItemsEl.append(createNewEl);
-  });
 }
 
 export function itemBtnAddToCart({ id, imgUrl, title, price, quantity = 1 }) {
@@ -107,49 +47,4 @@ export function getItemsFromStorage() {
 
 function setItemsToStorage(items = []) {
   window.localStorage.setItem("cart", JSON.stringify(items));
-}
-
-function clearAllItemsFromCart() {
-  setItemsToStorage([]);
-  renderCartItems([]);
-}
-
-function removeItemFromCart(items = [], productId) {
-  const temp = items.filter((item) => item.id != productId);
-
-  setItemsToStorage(temp);
-
-  renderCartItems(temp);
-}
-
-function renderCartPrice(items = []) {
-  clearNode(totalCartPriceEl);
-
-  items.forEach(({ price, quantity }) => {
-    const template = totalPricetemplate({
-      price,
-      quantity,
-    });
-    const createNewEl = createHTML(template);
-    const total = calcTotalPrice(createNewEl);
-
-    totalCartPriceEl.append(total);
-  });
-}
-
-function totalPricetemplate({ price }) {
-  return `
-  
-  <span>${price} ${CURRENCY}</span>
-  `;
-}
-
-function calcTotalPrice(items = []) {
-  const calculateQty = items.reduce((total, item) => {
-    return total + item.quantity;
-  });
-
-  getItemsFromStorage(calculateQty);
-
-  renderCartPrice(calculateQty);
 }
--- src/index.mjs
+++ src/index.mjs
@@ -2,11 +2,8 @@ import { APIURL, ERROR_IF_NOT_GETTING_DATA } from "./constants.mjs";
 import { createHTML, clearNode } from "./utils.mjs";
 import { productTemplate } from "./products/productTemplate.mjs";
 import { loadingSkeleton } from "./products/skeletonTemplate.mjs";
-import { itemBtnAddToCart } from "./cart.mjs";
 
 const containerProductsEl = document.querySelector("#js-products-render");
-const sortPriceEl = document.querySelector("#js-sort-price");
-const filterGenderEl = document.querySelector("#js-filter-gender");
 
 //Gotta put em products in a list
 let allProducts = [];
@@ -19,7 +16,7 @@ let allProducts = [];
 setup();
 
 async function setup() {
-  if (!containerProductsEl || !sortPriceEl || !filterGenderEl) {
+  if (!containerProductsEl) {
     console.error("JS is CRACKED!");
   } else {
     loadingSkeleton(containerProductsEl);
@@ -31,43 +28,6 @@ async function setup() {
     containerProductsEl.addEventListener("click", onProductClick);
   }
 }
-
-filterGenderEl.addEventListener("change", (event) => {
-  clearNode(containerProductsEl);
-  loadingSkeleton(containerProductsEl);
-
-  const val = event.target.value;
-
-  if (val === "men") {
-    const maleJackets = allProducts.filter(
-      (jacket) => jacket.gender === "Male"
-    );
-    renderProductsListEl(maleJackets);
-  } else if (val === "women") {
-    const femaleJackets = allProducts.filter(
-      (jacket) => jacket.gender === "Female"
-    );
-    renderProductsListEl(femaleJackets);
-  } else if (val === "gender") {
-    const defaultGender = allProducts;
-    renderProductsListEl(defaultGender);
-  }
-});
-
-sortPriceEl.addEventListener("change", (event) => {
-  clearNode(containerProductsEl);
-  loadingSkeleton(containerProductsEl);
-
-  const val = event.target.value;
-
-  if (val === "LowHigh") {
-    highToLow();
-  } else if (val === "HighLow") {
-    lowToHigh();
-  }
-
-  renderProductsListEl(allProducts);
-});
 
 // fetching (getting) products from an API with an async function
 async function fetchingProducts() {
@@ -92,12 +52,12 @@ async function fetchingProducts() {
 function renderProductsListEl(list = []) {
   clearNode(containerProductsEl);
 
-  list.forEach(({ id, title, image, price, description }) => {
+  list.forEach(({ id, title, images, price, description }) => {
     const template = productTemplate({
       id: id,
       title: title,
-      imgUrl: image.url,
-      imgAl: image.alt,
+      imgUrl: images,
+      imgAl: images,
       price: price,
       description: description,
     });
@@ -106,37 +66,4 @@ function renderProductsListEl(list = []) {
 
     containerProductsEl.append(createNewEl);
   });
-}
-
-function highToLow(list = allProducts) {
-  list.sort((a, b) => a.price - b.price);
-}
-
-function lowToHigh(list = allProducts) {
-  list.sort((a, b) => b.price - a.price);
-}
-
-// The on product is heavily influenced by Monde Sineke (My teacher) :)
-
-export function onProductClick(event) {
-  const target = event.target;
-
-  const container = target.closest("[data-component='productPreviewDetails']");
-  const productId = container?.dataset?.productid;
-
-  if (target.tagName === "BUTTON" && container) {
-    const products = allProducts;
-    const selectedProduct = products.find((p) => p.id === productId);
-
-    if (selectedProduct) {
-      itemBtnAddToCart({
-        id: selectedProduct.id,
-        title: selectedProduct.title,
-        imgUrl: selectedProduct.image.url,
-        price: selectedProduct.price,
-      });
-    }
-  } else if (target.tagName === "IMG" && container) {
-    console.error(`Navigate to product details for product ID: ${productId}`);
-  }
 }
